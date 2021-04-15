8e28eb2f542b8e056515b401b15847583f417a50
Gauthier Bastien <g.bastien@imio.be>
2021-04-15 12:26:58 +0200

    Translate error messages, especially message regarding CORS issue

--- lang/et.js
+++ lang/et.js
@@ -1,4 +1,7 @@
 ﻿CKEDITOR.plugins.setLang('imagerotate', 'et', {
   rotateRight: 'Pööra päripäeva',
-  rotateLeft: 'Pööra vastupäeva'
+  rotateLeft: 'Pööra vastupäeva',
+  errorNoImage: "no image element?",
+  errorNoDOMImage: "no DOM image element?",
+  errorImageFromOtherDomain: "Image is from other domain and can't be rotated",
 });
--- lang/fr.js
+++ lang/fr.js
@@ -1,4 +1,7 @@
 CKEDITOR.plugins.setLang('imagerotate', 'fr', {
   rotateRight: 'Rotation horaire',
-  rotateLeft: 'Rotation antihoraire'
+  rotateLeft: 'Rotation antihoraire',
+  errorNoImage: "no image element?",
+  errorNoDOMImage: "no DOM image element?",
+  errorImageFromOtherDomain: "Image is from other domain and can't be rotated",
 });
